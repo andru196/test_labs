f4084c750dc259654447acc5d0ad66d22170e8e7
andru196 <andru196@icloud.com>
2019-04-27 11:57:55 +0300

    last

--- 04/02/aaa.sh
+++ 04/02/aaa.sh
@@ -1,3 +1,8 @@
+DATENOW0=$(date +%s)
+DATENOW0=$(expr $DATENOW0 + 10)
+DATENOW0=$(date -d @$DATENOW0 "+20%y%m%d%H%M.%S")
+rm bomb.txt
 touch bomb.txt
-touch -A "000010" bomb.txt
+touch -mat $DATENOW0 bomb.txt
+
 
--- 04/02/defuse.sh
+++ 04/02/defuse.sh
@@ -1,2 +1,12 @@
-touch -A '-000001' "bomb.txt"
-stat -s bomb.txt | awk {'print $10'} | sed "s/.*ime=//"
+NOWTIME0=$(date '+%s')
+TIMEBANG=$(stat -c "%X" bomb.txt)
+while [ $TIMEBANG -ne $NOWTIME0 ]
+do
+	TIMEBANG=$(expr $(stat -c "%X" bomb.txt) - 1)
+	MYTIME=$(date -d @$TIMEBANG "+20%y%m%d%H%M.%S")
+	touch -mat $MYTIME bomb.txt
+	TIMESS=$(expr $TIMEBANG - $NOWTIME0)
+	echo $TIMESS
+	sleep 1
+done
+echo LOL
